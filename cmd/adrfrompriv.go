5df863b917d2ef056c7fe5ee97d06059daba683b
kubasiemion <kuba.siemion@gmail.com>
2021-11-22 12:51:06 +0100

    Work in progress

---
+++ cmd/adrfrompriv.go
@@ -0,0 +1,62 @@
+package cmd
+
+import (
+	"encoding/hex"
+	"fmt"
+	"math/big"
+
+	"github.com/btcsuite/btcd/btcec"
+	"github.com/proveniencenft/kmsclitool/common"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(adrFromPrivCmd)
+	adrFromPrivCmd.Flags().StringVarP(&privhex, "privkey", "p", "", "--privkey privkey_in_hex")
+}
+
+var privhex string
+
+var adrFromPrivCmd = &cobra.Command{
+	Use:   "adrFromPriv",
+	Short: "derive address from a given private key",
+	Long:  "long",
+	Run:   adrFromPriv,
+}
+
+func adrFromPriv(cmd *cobra.Command, args []string) {
+	if len(privhex) == 0 {
+		fmt.Println("No private key given")
+		return
+	}
+
+	if len(privhex) > 2 && privhex[:2] == "0x" {
+		privhex = privhex[2:]
+	}
+	priv, err := hex.DecodeString(privhex)
+	if err != nil {
+		fmt.Println("Error decoding the private key:", err)
+		return
+	}
+	_, pub := btcec.PrivKeyFromBytes(btcec.S256(), priv)
+	kecc := common.Keccak256(append(pub.X.Bytes(), pub.Y.Bytes()...))
+	addr := kecc[12:]
+	fmt.Println(hex.EncodeToString(addr))
+
+	x, y := btcec.S256().ScalarBaseMult(priv)
+	fmt.Println("x:", hex.EncodeToString(x.Bytes()))
+	fmt.Println("y:", hex.EncodeToString(y.Bytes()))
+	kecc = common.Keccak256(append(Padd(x), Padd(y)...))
+	addr = kecc[12:]
+	fmt.Println(hex.EncodeToString(addr))
+}
+
+func Padd(s *big.Int) []byte {
+	xb := s.Bytes()
+	if len(xb) < 32 {
+		fmt.Println("Padding")
+		fill := make([]byte, 32-len(xb))
+		xb = append(fill, xb...)
+	}
+	return xb
+}
